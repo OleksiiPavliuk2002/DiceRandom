fde1126df3655ae59afa73c48a64fd729a84027a
Oleksii Pavliuk <141736751+OleksiiPavliuk2002@users.noreply.github.com>
2025-09-26 14:44:13 +0300

    Update data.js

--- constants/data.js
+++ constants/data.js
@@ -1,1 +1,10 @@
+const dice = {
+  1: require("../assets/dice1.png"),
+  2: require("../assets/dice2.png"),
+  3: require("../assets/dice3.png"),
+  4: require("../assets/dice4.png"),
+  5: require("../assets/dice5.png"),
+  6: require("../assets/dice6.png"),
+};
 
+export { dice };
